5168829ac39b5777515832c433e6c1296bb0d3a3
Stephen L. De Rudder <sldr@users.noreply.github.com>
2025-12-10 04:14:22 -0600

    Set flash size to 4MB, add partitions.csv, and initialize NVS.

--- main/SLDRDemo.c
+++ main/SLDRDemo.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <esp_log.h>
+#include <nvs_flash.h>
 
 #define TAG "SLDRDemo"
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "app_main starting");
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_LOGI(TAG, "nvs_flash_init completed");
+
     ESP_LOGI(TAG, "app_main finished");
 }
