2fb8d32657a8d8708163825f17979065c5292376
Stephen L. De Rudder <sldr@users.noreply.github.com>
2025-12-11 18:15:11 -0600

    Convert to C++ source file and update CMakeLists.txt accordingly.

--- main/SLDRDemo.cpp
+++ main/SLDRDemo.cpp
@@ -50,16 +50,15 @@ static void configure_led(void)
         .strip_gpio_num = CONFIG_BSP_LED_RGB_GPIO,
         .max_leds = 1, // at least one LED on board
     };
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
-    };
+    led_strip_rmt_config_t rmt_config = {};
+    rmt_config.resolution_hz = 10 * 1000 * 1000; // 10MHz
+    rmt_config.flags.with_dma = false;
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
     /* Set all LED off to clear all pixels */
     led_strip_clear(led_strip);
 }
 
-void app_main(void)
+extern "C" void app_main(void)
 {
     ESP_LOGI(TAG, "app_main starting");
 
